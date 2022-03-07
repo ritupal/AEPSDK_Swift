44ce6a87478e7bf027d947f1b14d48fdeea8c1d4
ritu pal <ritugrd4@gmail.com>
2022-03-07 18:36:17 +0530

    Initial Commit

---
+++ AEPSDK_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AEPSDK_Swift
+//
+//  Created by Kumari Ritu Pal on 07/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
